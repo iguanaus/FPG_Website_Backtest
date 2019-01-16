dac362b65ef03e687580844fad35b0b5b8929770
iguanaus4@yahoo.com <johnpeurifoy@yahoo.com>
2019-01-16 14:23:27 -0500

    enw

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,5 +2,5 @@
 
 cd /home/ec2-user/www/project/
 source /home/ec2-user/www/project-venv/bin/activate
-echo yes | DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here JWT_SECRET_KEY=your-jwt-secret-here PSQL_DB_NAME=your-db-name-here PSQL_DB_USER=your-db-user-here PSQL_DB_PASSWD=your-db-password-here PSQL_HOST=your-aws-psql-rds-server-dns-here PSQL_PORT=5432 /home/ec2-user/www/project/manage.py collectstatic
-DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here JWT_SECRET_KEY=your-jwt-secret-here PSQL_DB_NAME=your-db-name-here PSQL_DB_USER=your-db-user-here PSQL_DB_PASSWD=your-db-password-here PSQL_HOST=your-aws-psql-rds-server-dns-here PSQL_PORT=5432 supervisord -c /home/ec2-user/www/project/supervisor/default.conf
+
+screen -dmS "test_screen" python /home/ec2-user/www/project/test_file.py
