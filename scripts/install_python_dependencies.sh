d597dcd291344d7edf97c2db6fdc535d623d47fc
iguanaus4@yahoo.com <johnpeurifoy@yahoo.com>
2019-01-16 15:38:33 -0500

    removed venve links

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 chown ec2-user:ec2-user /home/ec2-user/www
-virtualenv /home/ec2-user/www/project-venv
-chown ec2-user:ec2-user /home/ec2-user/www/project-venv
-chown ec2-user:ec2-user /home/ec2-user/www/project-venv/*
