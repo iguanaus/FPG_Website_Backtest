c6025630b4817553101e6ce84814e6b877bd0524
iguanaus4@yahoo.com <johnpeurifoy@yahoo.com>
2019-01-16 15:35:40 -0500

    all new files

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -3,4 +3,3 @@ chown ec2-user:ec2-user /home/ec2-user/www
 virtualenv /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv/*
-source /home/ec2-user/www/project-venv/bin/activate
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/www/project/
-source /home/ec2-user/www/project-venv/bin/activate
 
 screen -dmS "test_screen" python /home/ec2-user/www/project/test_file.py
