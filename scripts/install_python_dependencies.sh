37bd443a7d6954a61a38596a23ff87ca78637fe1
iguanaus4@yahoo.com <johnpeurifoy@yahoo.com>
2019-01-16 15:11:21 -0500

    new

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -4,4 +4,3 @@ virtualenv /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv
 chown ec2-user:ec2-user /home/ec2-user/www/project-venv/*
 source /home/ec2-user/www/project-venv/bin/activate
-pip install -r /home/ec2-user/www/project/requirements/staging.txt
